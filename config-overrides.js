2f10a4c7b7048ec03f43b9069b7588df2ed30022
yishuihe <lixiang89@163.com>
2022-01-27 15:24:00 +0800

    page

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-const { override, addLessLoader } = require('customize-cra')
+const { override, addWebpackPlugin,setWebpackPublicPath } = require('customize-cra')
 // paths.appBuild = path.join(path.dirname(paths.appBuild), 'dist');
 // config.output.path = path.join(path.dirname(config.output.path), 'dist');
 
@@ -6,7 +6,9 @@ const path = require('path')
 const paths = require('react-scripts/config/paths')
 paths.appBuild = path.join(path.dirname(paths.appBuild), 'docs')
 
-module.exports = override()
+module.exports = override(
+    setWebpackPublicPath('/antd-extend')
+)
 
 
 
